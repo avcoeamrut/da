1b08d406637be28f4e7b8c9f539e04eb2dc85bad
avcoeamrut <kardiletejas2003@gmail.com>
2024-11-06 08:16:14 +0530

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract BankAccount {
-  address public accountholder;
-    constructor()
-    {
-  accountholder=msg.sender;
+contract SimpleBank{
+    address public owner;
+    uint256 public balance;
+    event Deposit(address indexed from,uint256 amount);
+    event Withdraw(address indexed to,uint256 amount);
+    constructor() {
+        owner=msg.sender;
+        balance=0;
+    }
+    function deposit(uint256 amount) public {
+        require(msg.sender == owner,"only the owner can deposite money");
+        balance += amount;
+        emit Deposit(msg.sender, amount);
+    }
+    function withdraw(uint256 amount) public {
+        require(msg.sender == owner,"only the owner can deposite money");
+        require(amount <= balance,"insufficient balance");
+        balance -= amount;
+        emit Withdraw(msg.sender,amount);
+    }
+    function getBalance() public view returns (uint256){
+        return balance;
     }
 
-//0xf8e81D47203A594245E36C48e151709F0C19fBe8
-   uint256 balance=0;
-   function deposite()public payable 
-   {
-    require(msg.value > 0, "Deposite amount should be greater than 0");
-    require(msg.sender==accountholder,"you are not account owner");
-    balance +=msg.value;
-   }
-   function withdraw() payable public {
-        require(balance > 0, "Withdrawal amount should be greater than zero");
-        require(msg.sender==accountholder,"you are not account owner");
-        payable(msg.sender).transfer(balance);
-        balance = 0;
-    }
-   function showbalance() public view returns(uint)
-   {
- require(msg.sender==accountholder,"you are not account owner");
-return balance;
-   }
+
 }
